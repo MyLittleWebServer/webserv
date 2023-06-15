ffa8bb8dba97efef485fd3e3ecfdaf0ad0ba9eb8
chanhihi <chanhihi55@gmail.com>
2023-06-15 17:50:32 +0900

    feat(prototype): complete echo server Co-authored-by: Clearsu <Clearsu@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -7,28 +7,20 @@ int main()
 {
     Server server;
     server.startServer(); 
-
-    /* init kqueue */
     Kqueue eventQueue;
 
-    // std::map<int, std::string> clients;     // map for client socket:data
-
-    /* add event for server socket */
     eventQueue.addEvent(server.getSocket());
     std::cout << "echo server started" << std::endl;
 
     EventHandler eventHandler(server.getSocket());
 
-    /* main loop */
     while (1)
     {
         int eventCount = eventQueue.newEvents();
         for (int i = 0; i < eventCount; ++i)
         {
-            // const struct kevent &currEvent = eventQueue.getEvent(i);
             eventHandler.setCurrentEvent(i);
-            eventHandler.errorOccuration();
-            eventHandler.branchCondition();
+            eventHandler.checkStatus();
         }
     }
     return (0);
