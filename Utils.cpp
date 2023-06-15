5781b4559c05738916a084780c1f205951f6faf8
chanhihi <chanhihi55@gmail.com>
2023-06-15 12:53:07 +0900

    feat(util): add disconnect client to utils

--- Utils.cpp
+++ Utils.cpp
@@ -1,9 +1,15 @@
-#include <cstdarg>
-#include <iostream>
+#include "Utils.hpp"
 
 // void exitWithPerror(const char *fileName, const char *lineCount, std::string errorName)
-void exitWithPerror(std::string errorName)
+void exitWithPerror(const std::string &msg)
 {
-	std::cout << errorName << std::endl;
+	std::cerr << msg << std::endl;
 	exit(EXIT_FAILURE);
+}
+
+void disconnectClient(int client_fd, std::map<int, std::string> &clients)
+{
+	std::cout << "client disconnected: " << client_fd << std::endl;
+	close(client_fd);
+	clients.erase(client_fd);
 }
--- Utils.hpp
+++ Utils.hpp
@@ -1,3 +1,10 @@
 #pragma once
 
-void exitWithPerror(std::string errorName);
+#include <map>
+#include <cstdarg>
+#include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
+
+void exitWithPerror(const std::string &msg);
+void disconnectClient(int client_fd, std::map<int, std::string> &clients);
