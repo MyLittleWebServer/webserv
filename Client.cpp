3f0eec42e736c96c7466efda5a425cd98d30406d
chanhihi <chanhihi55@gmail.com>
2023-06-19 17:21:01 +0900

    wip(Client): testing GET method to work

--- Client.cpp
+++ Client.cpp
@@ -1,87 +1,76 @@
 #include "Client.hpp"
 
-Client::Client() : _sd(0), _method(NULL)
-{
+char Client::_buf[TRANSFER_LEN + 1] = {0};
+
+Client::Client() : _sd(0), _method(NULL) {}
+
+Client::Client(const uintptr_t sd) : _sd(sd), _method(NULL) {}
+
+Client::~Client(void) {
+  if (this->_method != NULL) delete this->_method;
 }
 
-Client::Client(const uintptr_t sd) : _sd(sd), _method(NULL)
-{
+void Client::receiveData(void) {
+  while (1) {
+    ssize_t n = recv(this->_sd, Client::_buf, TRANSFER_LEN, 0);
+    // std::cout << "n: " << n << '\n'; zoosuck
+    if (n <= 0) {
+      if (n < 0) throw std::runtime_error("Client read error!");
+      throw std::runtime_error("Client disconnected!");
+    }
+    Client::_buf[n] = '\0';
+    this->_request += Client::_buf;
+    std::memset(Client::_buf, 0, TRANSFER_LEN + 1);
+    if (n < TRANSFER_LEN ||
+        recv(this->_sd, Client::_buf, TRANSFER_LEN, MSG_PEEK) == -1) {
+      std::cout << "received data from " << this->_sd << ": " << this->_request
+                << std::endl;
+      return;
+    }
+  }
 }
 
-Client::~Client(void)
-{
-	if (this->_method != NULL)
-		delete this->_method;
+void Client::makeResponse(void) {
+  this->_method->parseRequest();
+  this->_method->createResponse();
 }
 
-void	Client::receiveData(void)
-{	
-	while (1)
-	{
-		ssize_t n = recv(this->_sd, this->_buf, TRANSFER_LEN, 0);
-		// std::cout << "n: " << n << '\n'; zoosuck
-		if (n <= 0)
-		{
-			if (n < 0)
-				throw std::runtime_error("Client read error!");
-			throw std::runtime_error("Client disconnected!");
-		}
-		this->_buf[n] = '\0';
-		this->_request += this->_buf;
-		std::memset(this->_buf, 0, TRANSFER_LEN + 1);
-		if (n < TRANSFER_LEN || recv(this->_sd, this->_buf, TRANSFER_LEN, MSG_PEEK) == -1)
-		{
-			std::cout << "received data from " << this->_sd << ": " << this->_request << std::endl;
-			return ;
-		}
-	}
+void Client::sendData(std::map<int, Client> &clients) {
+  std::map<int, Client>::iterator it = clients.find(this->_sd);
+  if (it != clients.end() && this->_method != NULL &&
+      this->_method->getResponseFlag() == true) {
+    const std::string &response = this->_method->getResponse();
+    std::string::const_iterator pos = response.begin();
+    std::string::const_iterator begin = response.begin();
+    std::string::const_iterator end = response.end();
+    while (1) {
+      ssize_t n =
+          send(this->_sd, response.c_str() + (pos - begin), TRANSFER_LEN, 0);
+
+      if (n == -1) throw std::runtime_error("Client send error!");
+      pos += n;
+      if (pos == end) {
+        this->_request.clear();
+        delete this->_method;
+        this->_method = NULL;
+        return;
+      }
+    }
+  }
 }
 
-void	Client::sendData(std::map<int, Client> &clients)
-{
-	std::map<int, Client>::iterator it = clients.find(this->_sd);
-	if (it != clients.end() && this->_method->getResponseFlag() == true)
-	{
-		const std::string &response = this->_method->getResponse();
-		std::string::const_iterator pos = response.begin();
-		std::string::const_iterator begin = response.begin();
-		std::string::const_iterator end = response.end();
-		while (1)
-		{
-			ssize_t n = send(this->_sd, \
-							response.c_str() + (pos - begin), \
-							TRANSFER_LEN, 0);
-
-			if (n == -1)
-				throw std::runtime_error("Client send error!");
-			pos += n;
-			if (pos == end)
-			{
-				this->_request.clear();
-				delete this->_method;
-				this->_method = NULL;
-				return ;
-			}
-		}
-	}
-}
-
-void	Client::newHTTPMethod(void)
-{
-	if (this->_request.compare(0, 4, "GET ") == 0)
-	{
-		this->_method = new GET(this->_request);
-		return ;
-	}
-	if (this->_request.compare(0, 5, "POST ") == 0)
-	{
-		this->_method = new POST(this->_request);
-		return ;
-	}
-	if (this->_request.compare(0, 7, "DELETE ") == 0)
-	{
-		this->_method = new DELETE(this->_request);
-		return ;
-	}
-	throw std::runtime_error("501 Not implemented");
+void Client::newHTTPMethod(void) {
+  if (this->_request.compare(0, 4, "GET ") == 0) {
+    this->_method = new GET(this->_request);
+    return;
+  }
+  if (this->_request.compare(0, 5, "POST ") == 0) {
+    this->_method = new POST(this->_request);
+    return;
+  }
+  if (this->_request.compare(0, 7, "DELETE ") == 0) {
+    this->_method = new DELETE(this->_request);
+    return;
+  }
+  throw std::runtime_error("501 Not implemented");
 }
--- Client.hpp
+++ Client.hpp
@@ -1,8 +1,8 @@
 #ifndef Client_HPP
-# define Client_HPP
+#define Client_HPP
 
-#include <sys/socket.h>
 #include <fcntl.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
 #include <iostream>
@@ -10,29 +10,30 @@
 #include <string>
 
 #include "./Clients/AMethod.hpp"
+#include "./Clients/DELETE.hpp"
 #include "./Clients/GET.hpp"
 #include "./Clients/POST.hpp"
-#include "./Clients/DELETE.hpp"
 
 #define TRANSFER_LEN 10
 
-class Client
-{
-private:
-	const uintptr_t	_sd;
-	std::string		_request;
-	AMethod			*_method;
+class Client {
+ private:
+  const uintptr_t _sd;
+  std::string _request;
+  AMethod *_method;
 
-	static char		_buf[TRANSFER_LEN + 1];
-public:
-	Client(void);
-	Client(const uintptr_t sd);
-	virtual ~Client(void);
-public:
-	void	receiveData(void);
-	void 	sendData(std::map<int, Client> &_clients);
-	void	newHTTPMethod(void);
+  static char _buf[TRANSFER_LEN + 1];
 
+ public:
+  Client(void);
+  Client(const uintptr_t sd);
+  virtual ~Client(void);
+
+ public:
+  void receiveData(void);
+  void makeResponse(void);
+  void sendData(std::map<int, Client> &_clients);
+  void newHTTPMethod(void);
 };
 
 #endif
