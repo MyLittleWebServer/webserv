cbb61c3115bd20620d6ac0287f6a4c5056c988dc
chanhihi <chanhihi55@gmail.com>
2023-06-22 23:44:18 +0900

    feat(AMethod): combine config add headerfield
    Co-authored-by: Clearsu <Clearsu@users.noreply.github.com>
    Co-authored-by: cjho0316 <cjho0316@users.noreply.github.com>

--- srcs/clients/method/src/AMethod.cpp
+++ srcs/clients/method/src/AMethod.cpp
@@ -1,8 +1,62 @@
 #include "AMethod.hpp"
 
+#include "errorMessage.hpp"
+
 AMethod::AMethod() {}
 AMethod::AMethod(std::string &request)
-    : _request(request), _responseFlag(false) {}
+    : _request(request), _responseFlag(false) {
+  /* major-field */
+  this->_headerFields["host"] = "";
+  this->_headerFields["accept"] = "";
+  this->_headerFields["accept-language"] = "";
+  this->_headerFields["accept-encoding"] = "";
+  this->_headerFields["accept-charset"] = "";
+  this->_headerFields["authorization"] = "";
+  this->_headerFields["content-type"] = "";
+  this->_headerFields["connection"] = "";
+  this->_headerFields["user-agent"] = "";
+  /* sub-field */
+  this->_headerFields["content-length"] = "";
+  this->_headerFields["content-language"] = "";
+  this->_headerFields["content-encoding"] = "";
+  this->_headerFields["content-range"] = "";
+  this->_headerFields["content-length"] = "";
+  this->_headerFields["content-base"] = "";
+  this->_headerFields["content-location"] = "";
+  this->_headerFields["content-range"] = "";
+  this->_headerFields["keep-alive"] = "";
+  this->_headerFields["referer"] = "";
+  this->_headerFields["cookie"] = "";
+  this->_headerFields["last-modified"] = "";
+  this->_headerFields["if-modified-since"] = "";
+  this->_headerFields["date"] = "";
+  this->_headerFields["server"] = "";
+  this->_headerFields["www-authenticate"] = "";
+  this->_headerFields["retry-after"] = "";
+  this->_headerFields["location"] = "";
+  this->_headerFields["content-md5"] = "";
+  this->_headerFields["cache-control"] = "";
+  this->_headerFields["pragma"] = "";
+  this->_headerFields["expires"] = "";
+  this->_headerFields["age"] = "";
+  this->_headerFields["allow"] = "";
+  this->_headerFields["etag"] = "";
+  this->_headerFields["accept-ranges"] = "";
+  this->_headerFields["set-cookie"] = "";
+  this->_headerFields["vary"] = "";
+  this->_headerFields["x-frame-options"] = "";
+  this->_headerFields["x-xss-protection"] = "";
+  this->_headerFields["x-content-type-options"] = "";
+  this->_headerFields["x-forwarded-for"] = "";
+  this->_headerFields["x-forwarded-host"] = "";
+  this->_headerFields["x-forwarded-server"] = "";
+  this->_headerFields["x-forwarded-proto"] = "";
+  this->_headerFields["x-real-ip"] = "";
+  this->_headerFields["x-request-id"] = "";
+  this->_headerFields["x-correlation-id"] = "";
+  this->_headerFields["x-csrf-token"] = "";
+  this->_headerFields["x-device-user-agent"] = "";
+}
 AMethod::~AMethod() {}
 
 void AMethod::splitLinesByCRLF(void) {
@@ -33,32 +87,58 @@ void AMethod::parseRequestLine(void) {
   std::istringstream iss(firstLine);
   this->_linesBuffer.pop_front();
   iss >> this->_method >> this->_path >> this->_protocol;
+
+  // std::cout << "method: " << this->_method << std::endl;
+  // std::cout << "path: " << this->_path << std::endl;
+  // std::cout << "protocol: " << this->_protocol << std::endl;
+  if (this->_method == "" || this->_path == "" || this->_protocol == "")
+    throw std::runtime_error("400 Bad Request");
 }
 
 void AMethod::parseHeaderFields(void) {
-  std::list<std::string>::const_iterator it = this->_linesBuffer.begin();
-  std::list<std::string>::const_iterator end = this->_linesBuffer.end();
+  std::list<std::string>::const_iterator lineIt = this->_linesBuffer.begin();
+  std::list<std::string>::const_iterator lineEnd = this->_linesBuffer.end();
 
   std::string key;
   std::string value;
 
   size_t pos = 0;
-  while (it != end) {
-    pos = (*it).find(": ");
-    key = (*it).substr(0, pos);
-    value = (*it).substr(pos + 2, (*it).size() - pos - 2);
+  while (lineIt != lineEnd) {
+    pos = (*lineIt).find(": ");
+    key = (*lineIt).substr(0, pos);
+    value = (*lineIt).substr(pos + 2, (*lineIt).size() - pos - 2);
+    // value 검증 필요
+    this->_headerFields[key] = value;
+    ++lineIt;
     this->_linesBuffer.pop_front();
-    this->_headerFields.insert(std::pair<std::string, std::string>(key, value));
-    ++it;
-    if (checkBodyExistance(*it)) {
+    if (checkBodyExistance(lineIt)) {
       this->_linesBuffer.pop_front();
       return;
     }
   }
 }
 
-bool AMethod::checkBodyExistance(const std::string &str) const {
-  return (str == "");
+bool AMethod::checkBodyExistance(
+    std::list<std::string>::const_iterator it) const {
+  return (!this->_linesBuffer.empty() && *it == "");
+}
+
+bool AMethod::checkPathForm() {
+  std::string::const_iterator iter = this->_path.begin();
+  std::string::const_iterator end = this->_path.end();
+  if (*iter == '/') ++iter;
+  while (iter != end) {
+    if (*iter == '/' && *(iter + 1) == '/') return false;
+    ++iter;
+  }
+  return true;
+}
+
+void AMethod::setDefaultLocation(
+    std::list<ILocationConfig *>::const_iterator defaultLocation) {
+  this->_matchedLocation = *defaultLocation;
+  this->_path = (*defaultLocation)->getRoot() +
+                this->_path.substr(1, this->_path.size() - 1);
 }
 
 const std::string &AMethod::getResponse(void) const {
@@ -66,3 +146,83 @@ const std::string &AMethod::getResponse(void) const {
 }
 
 bool AMethod::getResponseFlag(void) const { return (this->_responseFlag); }
+
+void AMethod::matchServerConf(short port) {
+  this->_matchedServer = NULL;
+  Config &config = Config::getInstance();
+  std::list<IServerConfig *> &serverInfo = config.getServerConfigs();
+  std::list<IServerConfig *>::iterator it = serverInfo.begin();
+  while (it != serverInfo.end()) {
+    if ((*it)->getListen() != port) {
+      ++it;
+      continue;
+    }
+    if ((*it)->getServerName() == this->_headerFields["host"]) {
+      this->_matchedServer = *it;
+      return;
+    } else if (this->_matchedServer == NULL)
+      this->_matchedServer = *it;
+    ++it;
+  }
+}
+
+// /root//dir/test.txt
+// GET /dir/test.txt/ hTML/1.1
+void AMethod::validatePath(void) {
+  size_t pos = this->_path.find("/", 1);
+  size_t end;
+  if (pos == std::string::npos)
+    end = 1;
+  else
+    end = pos;
+  std::string firstToken = this->_path.substr(0, end);
+  std::list<ILocationConfig *>::const_iterator it =
+      this->_matchedServer->getLocationConfigs().begin();
+  std::list<ILocationConfig *>::const_iterator endIt =
+      this->_matchedServer->getLocationConfigs().end();
+  std::list<ILocationConfig *>::const_iterator defaultLocation;
+  while (it != endIt) {
+    std::string &currRoute = (*it)->getRoute();
+    if (currRoute == firstToken) {
+      this->_matchedLocation = *it;
+      if (end == pos) end++;
+      this->_path =
+          currRoute + this->_path.substr(end, this->_path.size() - end);
+      if (this->checkPathForm() == false) this->_statusCode = 404;
+      return;
+    }
+    if (currRoute == "/") defaultLocation = it;
+    ++it;
+  }
+  this->setDefaultLocation(defaultLocation);
+}
+
+void AMethod::parseRequest(void) {
+  this->splitLinesByCRLF();
+  this->parseRequestLine();
+  this->parseHeaderFields();
+}
+
+void AMethod::createResponse(void) {
+  std::stringstream ss;
+  ss << this->_statusCode;
+  this->_response += "HTTP/1.1" + ss.str();
+  if (this->_statusCode == 404) {  // TODO: map으로 합시다. key값으로 들어가고
+    this->_response += errMSG404;
+    this->_response += "Content-Type: text/html; charset=UTF-8\r\n";
+    this->_response += "\r\n";
+    this->_response += "<html><body><h1>404 Not Found</h1></body></html>\r\n";
+    return;
+  } else {
+    this->_response += "HTTP/1.1 " + ss.str() + "\r\n";
+    this->_response += " OK\r\n";  // ss.str(); 받아오는 녀석
+    this->_response += "Content-Type: text/html; charset=UTF-8\r\n";
+  }
+
+  GET *child = dynamic_cast<GET *>(this);
+  if (child != NULL)
+    child->appendFileContent();
+  else if (_method == "POST") {
+    this->_response += "\r\n";
+  }
+}
