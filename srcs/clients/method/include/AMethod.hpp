8b84ac2322ec095dfb91f6b7d02c97a513ca0a2f
chanhihi <chanhihi55@gmail.com>
2023-06-22 23:42:51 +0900

    feat(AMethod): combine config
    Co-authored-by: Clearsu <Clearsu@users.noreply.github.com>
    Co-authored-by: cjho0316 <cjho0316@users.noreply.github.com>

--- srcs/clients/method/include/AMethod.hpp
+++ srcs/clients/method/include/AMethod.hpp
@@ -6,6 +6,9 @@
 #include <sstream>
 #include <string>
 
+#include "Config.hpp"
+#include "GET.hpp"
+
 class AMethod {
  protected:
   std::string _request;
@@ -15,21 +18,34 @@ class AMethod {
   std::string _method;
   std::string _path;
   std::string _protocol;
+  std::string _CGI;
+  int _statusCode;
 
   std::list<std::string> _linesBuffer;
   std::map<std::string, std::string> _headerFields;
+  std::map<std::string, std::string> _serverConf;
+  IServerConfig *_matchedServer;
+  ILocationConfig *_matchedLocation;
 
   void splitLinesByCRLF(void);
   void parseRequestLine(void);
   void parseHeaderFields(void);
-  bool checkBodyExistance(const std::string &str) const;
+
+  bool checkBodyExistance(std::list<std::string>::const_iterator it) const;
+  bool checkPathForm();
+  void setDefaultLocation(
+      std::list<ILocationConfig *>::const_iterator defaultLocation);
 
  public:
   AMethod();
   AMethod(std::string &request);
   virtual ~AMethod();
-  virtual void parseRequestMessage(void) = 0;
-  virtual void createResponseMessage(void) = 0;
+
+  void parseRequest(void);
+  void matchServerConf(short port);
+  void validatePath(void);
+  virtual void doRequest(void) = 0;
+  void createResponse(void);
 
   const std::string &getResponse(void) const;
   bool getResponseFlag(void) const;
