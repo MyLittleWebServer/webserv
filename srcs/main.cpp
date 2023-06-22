3159bbdf16fc61e137652f17ecc02832a0a23d4d
chanhihi <chanhihi55@gmail.com>
2023-06-22 23:38:44 +0900

    feat(error): implement exception

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -11,28 +11,33 @@ int main(int ac, char** av) {
 #ifdef LEAKS
   atexit(leakCheck);
 #endif
-  if (ac > 2) {
-    std::cout << "Usage: ./webserv <config_file(optional)>" << std::endl;
-    return (1);
-  }
-  Config& config = Config::getInstance("config/default.conf");
-  (void)config;
-  (void)av;
-  Server server;
-  server.startServer();
-  Kqueue eventQueue;
-
-  eventQueue.addEvent(server.getSocket());
-  std::cout << "echo server started" << std::endl;
-
-  EventHandler eventHandler(server.getSocket());
-
-  while (1) {
-    int eventCount = eventQueue.newEvents();
-    for (int i = 0; i < eventCount; ++i) {
-      eventHandler.setCurrentEvent(i);
-      eventHandler.checkStatus();
+  try {
+    if (ac > 2) {
+      std::cout << "Usage: ./webserv <config_file(optional)>" << std::endl;
+      return (1);
     }
+    Config& config = Config::getInstance("config/default.conf");
+    (void)config;
+    (void)av;
+    Server server;
+    server.startServer();
+    Kqueue eventQueue;
+
+    eventQueue.addEvent(server.getSocket());
+    std::cout << "echo server started" << std::endl;
+
+    EventHandler eventHandler(server.getSocket());
+
+    while (1) {
+      int eventCount = eventQueue.newEvents();
+      for (int i = 0; i < eventCount; ++i) {
+        eventHandler.setCurrentEvent(i);
+        eventHandler.checkStatus();
+      }
+    }
+
+  } catch (...) {
+    std::cout << "fatal error" << std::endl;
   }
   return (0);
 }
