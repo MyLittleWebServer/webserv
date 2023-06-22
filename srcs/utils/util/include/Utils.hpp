816da0ef39a729d288fb606e043294fa929da2fe
chanhihi <chanhihi55@gmail.com>
2023-06-22 23:32:14 +0900

    feat(port): find port by currentEvent

--- srcs/utils/util/include/Utils.hpp
+++ srcs/utils/util/include/Utils.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
-#include <map>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
 #include <cstdarg>
 #include <iostream>
-#include <fcntl.h>
-#include <unistd.h>
+#include <map>
 
 #include "Client.hpp"
 
 void exitWithPerror(const std::string &msg);
 void disconnectClient(int client_fd, std::map<int, Client> &clients);
+short getBoundPort(const struct kevent *_currentEvent);
--- srcs/utils/util/src/Utils.cpp
+++ srcs/utils/util/src/Utils.cpp
@@ -10,3 +10,12 @@ void disconnectClient(int client_fd, std::map<int, Client> &clients) {
   clients.erase(client_fd);
   std::cout << "Client " << client_fd << " disconnected!" << std::endl;
 }
+
+short getBoundPort(const struct kevent *_currentEvent) {
+  struct sockaddr_in addr;
+  socklen_t addr_len = sizeof(addr);
+  if (getsockname(_currentEvent->ident, (struct sockaddr *)&addr, &addr_len) ==
+      -1)
+    throw std::runtime_error("getsockname() error\n");
+  return (ntohs(addr.sin_port));
+}
