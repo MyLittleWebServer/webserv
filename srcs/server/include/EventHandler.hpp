0eca3835acc7590f5e6c0f9d2cf19bc33a1eb955
chanhihi <chanhihi55@gmail.com>
2023-06-22 23:33:51 +0900

    chore(lint): Kqueue, EventHandler

--- srcs/server/include/EventHandler.hpp
+++ srcs/server/include/EventHandler.hpp
@@ -1,42 +1,43 @@
 #ifndef EventHandler_HPP
-# define EventHandler_HPP
+#define EventHandler_HPP
 
-#include <sys/types.h>
-#include <sys/event.h>
-#include <sys/time.h>
 #include <arpa/inet.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <sys/event.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include <iostream>
 #include <map>
 #include <vector>
 
+#include "Client.hpp"
 #include "Kqueue.hpp"
 #include "Utils.hpp"
-#include "Client.hpp"
 
 class Client;
 
-class EventHandler : public Kqueue
-{
-private:
-	std::map<int, Client> _clients;
-	const uintptr_t _serverSocket;
-	struct kevent *_currentEvent;
+class EventHandler : public Kqueue {
+ private:
+  std::map<int, Client> _clients;
+  const uintptr_t _serverSocket;
+  struct kevent* _currentEvent;
 
-	void	acceptClient();
-	void	registClient(const uintptr_t clientSocket);
-public :
-	EventHandler(uintptr_t serverSocket);
-	EventHandler(const EventHandler& src);
-	virtual ~EventHandler();
-	EventHandler& operator=(EventHandler const& rhs);
+  void acceptClient();
+  void registClient(const uintptr_t clientSocket);
 
-	void	checkStatus();
-	void	checkErrorOnSocket();
-	void	setCurrentEvent(int i);
-	void	branchCondition();
+ public:
+  EventHandler(uintptr_t serverSocket);
+  EventHandler(const EventHandler& src);
+  virtual ~EventHandler();
+  EventHandler& operator=(EventHandler const& rhs);
+
+  void checkStatus();
+  void checkErrorOnSocket();
+  void setCurrentEvent(int i);
+  void branchCondition();
 };
 
 #endif
--- srcs/server/src/Kqueue.cpp
+++ srcs/server/src/Kqueue.cpp
@@ -4,41 +4,37 @@ int Kqueue::_kq = 0;
 std::vector<struct kevent> Kqueue::_eventsToAdd = std::vector<struct kevent>();
 struct kevent Kqueue::_eventList[CONCURRENT_EVENTS] = {};
 
-Kqueue::Kqueue(void)
-{
-	if ((this->_kq = kqueue()) == -1)
-		exitWithPerror("kqueue() error\n" + std::string(strerror(errno)));
+Kqueue::Kqueue(void) {
+  if ((this->_kq = kqueue()) == -1)
+    exitWithPerror("kqueue() error\n" + std::string(strerror(errno)));
 }
 
 Kqueue::~Kqueue() {}
 
-void Kqueue::addEvent(uintptr_t ident, int16_t filter, uint16_t flags, uint32_t fflags,
-					  intptr_t data, void *udata)
-{
-	struct kevent temp_event;
+void Kqueue::addEvent(uintptr_t ident, int16_t filter, uint16_t flags,
+                      uint32_t fflags, intptr_t data, void *udata) {
+  struct kevent temp_event;
 
-	EV_SET(&temp_event, ident, filter, flags, fflags, data, udata);
-	this->_eventsToAdd.push_back(temp_event);
+  EV_SET(&temp_event, ident, filter, flags, fflags, data, udata);
+  this->_eventsToAdd.push_back(temp_event);
 }
 
-void Kqueue::addEvent(uintptr_t ident)
-{
-	struct kevent temp_event;
+void Kqueue::addEvent(uintptr_t ident) {
+  struct kevent temp_event;
 
-	EV_SET(&temp_event, ident, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
-	this->_eventsToAdd.push_back(temp_event);
+  EV_SET(&temp_event, ident, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
+  this->_eventsToAdd.push_back(temp_event);
 }
 
-int Kqueue::newEvents()
-{
-	int new_events = kevent(_kq, &_eventsToAdd[0], _eventsToAdd.size(), _eventList, CONCURRENT_EVENTS, NULL);
-	if (new_events == -1)
-		exitWithPerror("kevent() error\n" + std::string(strerror(errno)));
-	_eventsToAdd.clear();
-	return (new_events);
+int Kqueue::newEvents() {
+  int new_events = kevent(_kq, &_eventsToAdd[0], _eventsToAdd.size(),
+                          _eventList, CONCURRENT_EVENTS, NULL);
+  if (new_events == -1)
+    exitWithPerror("kevent() error\n" + std::string(strerror(errno)));
+  _eventsToAdd.clear();
+  return (new_events);
 }
 
-const struct kevent &Kqueue::getEvent(int index) const
-{
-	return (this->_eventList[index]);
+const struct kevent &Kqueue::getEvent(int index) const {
+  return (this->_eventList[index]);
 }
