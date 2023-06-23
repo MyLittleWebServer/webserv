ba9ac96533a1b6aff8b4d19acf16290371a04ba9
Clearsu <clearwater1219@gmail.com>
2023-06-23 21:45:57 +0900

    feat(Utils): add getCurrentTime()
    Co-authored-by: KIM CHAN HEE <chanhihi@users.noreply.github.com>
    Co-authored-by: cjho0316 <cjho0316@users.noreply.github.com>

--- srcs/utils/util/src/Utils.cpp
+++ srcs/utils/util/src/Utils.cpp
@@ -1,5 +1,7 @@
 #include "Utils.hpp"
 
+#include <ctime>
+
 void exitWithPerror(const std::string &msg) {
   std::cerr << msg << std::endl;
   exit(EXIT_FAILURE);
@@ -18,4 +20,17 @@ short getBoundPort(const struct kevent *_currentEvent) {
       -1)
     throw std::runtime_error("getsockname() error\n");
   return (ntohs(addr.sin_port));
+}
+
+std::string getCurrentTime() {
+  std::time_t t = std::time(NULL);  // 현재 시간을 얻습니다.
+  std::tm *timePtr = std::gmtime(&t);  // 현재 시간을 UTC/GMT로 변환합니다.
+
+  char buffer[1000];
+  strftime(buffer, sizeof(buffer), "Date: %a, %d %b %Y %H:%M:%S GMT",
+           timePtr);  // 시간을 원하는 형식으로 포맷팅합니다.
+
+  std::string date(buffer);  // 변환된 문자열을 std::string에 저장합니다.
+
+  return (date);
 }
