cb0a80ce86f589f6d38f7f176c21086825d2acff
chanhihi <chanhihi55@gmail.com>
2023-06-26 00:24:09 +0900

    feat(main): main Validator
    Co-authored-by: cjho0316 <cjho0316@users.noreply.github.com>
    Co-authored-by: Clearsu <Clearsu@users.noreply.github.com>

--- srcs/utils/util/include/Utils.hpp
+++ srcs/utils/util/include/Utils.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <fcntl.h>
-#include <sys/socket.h>
 #include <unistd.h>
 
 #include <cstdarg>
@@ -10,8 +9,9 @@
 
 #include "Client.hpp"
 
-void exitWithPerror(const std::string &msg);
-void disconnectClient(int client_fd, std::map<int, Client> &clients);
+void throwWithPerror(const std::string &msg);
+void disconnectClient(int client_fd, std::map<int, Client *> &clients);
 short getBoundPort(const struct kevent *_currentEvent);
 std::string getCurrentTime();
 std::string itos(int num);
+void mainValidator(int ac, char **av);
