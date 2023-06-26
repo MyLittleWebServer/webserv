ca0545d36ca2ad2af44b74c83f1d1790fbadd502
chanhihi <chanhihi55@gmail.com>
2023-06-26 22:53:05 +0900

    feat(Server): Abstract Server from .conf
    Co-authored-by: Clearsu <Clearsu@users.noreply.github.com>
    Co-authored-by: cjho0316 <cjho0316@users.noreply.github.com>

--- srcs/server/include/Server.hpp
+++ srcs/server/include/Server.hpp
@@ -1,10 +1,12 @@
-#ifndef Server_HPP
-#define Server_HPP
+#pragma once
 
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-#define WAITLISTSIZE 10
+#include <set>
+#include <vector>
+
+#define BACKLOG 128
 
 class Server {
   typedef struct sockaddr_in sAddr_t;
@@ -12,24 +14,22 @@ class Server {
  private:
   sAddr_t _addr;
   int _socket;
-  int _waitListSize;
   int _port;
 
- public:
-  Server(void);
-  Server(int port);
-  virtual ~Server(void);
-
- public:
-  uintptr_t getSocket(void) const;
-
- public:
   void socketInit(void);
   void addrInit(void);
   void bindSocketWithAddr(void);
   void listenSocket(void) const;
   void asyncSocket(void) const;
-  void startServer(void);
-};
 
-#endif
+ public:
+  Server(int port);
+  virtual ~Server(void);
+
+ public:
+  uintptr_t getSocket(void) const;
+  short getPort(void) const;
+
+ public:
+  void initServerSocket(void);
+};
