aa943fd14b17777296af166fe08b0eb5eaa2a28e
chanhihi <chanhihi55@gmail.com>
2023-06-28 22:21:29 +0900

    feat(GET): reorganize content type with mimetypes

--- srcs/clients/method/include/GET.hpp
+++ srcs/clients/method/include/GET.hpp
@@ -6,14 +6,14 @@
 class GET : public AMethod {
  private:
   std::string _body;
-  bool _autoIndex;
+  std::string _contentType;
 
   std::vector<std::string> getFileList(const std::string& path);
   std::string generateHTML(const std::vector<std::string>& files);
   void prepareFileList(const std::string& path);
   void prepareBody(const std::string& pathIndex);
 
-  std::string getContentType(const std::string& path);
+  void getContentType(const std::string& path);
 
  public:
   GET();
