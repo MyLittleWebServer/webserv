0c95f4391e00450e3f45e81ed0451e4bd423ef1c
KIM CHAN HEE <85754295+chanhihi@users.noreply.github.com>
2023-07-01 18:25:40 +0900

    fix(GET): handling binary file
    * feat(GET): binary file handling
    
    * test(asset): add icon for test
    
    * test: add for test

--- srcs/clients/method/include/GET.hpp
+++ srcs/clients/method/include/GET.hpp
@@ -10,11 +10,14 @@ class GET : public AMethod {
 
   std::vector<std::string> getFileList(const std::string& path);
   std::string generateHTML(const std::vector<std::string>& files);
+  std::string validateContentType();
   void prepareFileList(const std::string& path);
   void prepareBody(const std::string& pathIndex);
-
   void getContentType(const std::string& path);
 
+  void prepareTextBody(const std::string& path);
+  void prepareBinaryBody(const std::string& path);
+  
  public:
   GET();
   GET(std::string& request);
