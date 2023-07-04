5b1371ea1e853432a6ac3480a4761fc9da810125
Clearsu <clearwater1219@gmail.com>
2023-07-04 22:07:47 +0900

    refactor(Response): add basic functions by changed structure

--- srcs/clients/response/include/IResponse.hpp
+++ srcs/clients/response/include/IResponse.hpp
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <string>
+
 class IResponse {
  public:
   virtual ~IResponse() {}
   virtual bool getResponseFlag(void) const = 0;
   virtual const std::string &getResponse(void) const = 0;
   virtual void assembleResponseLine(void) = 0;
+  virtual void addResponse(std::string msg) = 0;
+  virtual void setResponseParsed() = 0;
+  virtual bool isParsed() = 0;
 };
