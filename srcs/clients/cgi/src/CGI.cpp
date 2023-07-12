74c20ab0935ce362411aaf1da620de77ba9f1657
middlefitting <wjdtmdcjf2@gmail.com>
2023-07-12 20:20:46 +0900

    fix(cgi): _env life cycle

--- srcs/clients/cgi/src/CGI.cpp
+++ srcs/clients/cgi/src/CGI.cpp
@@ -43,53 +43,45 @@ CGI& CGI::operator=(const CGI& copy) {
 
 void CGI::initEnv() {
   _env.clear();
-  std::string content_type =
-      "CONTENT_TYPE=" + _request->getHeaderField("CONTENT-TYPE");
-  std::string content_length =
+  _content_type = "CONTENT_TYPE=" + _request->getHeaderField("CONTENT-TYPE");
+  _content_length =
       "CONTENT_LENGTH=" + std::to_string(_request->getContentLength());
-  std::string http_user_agent =
+  _http_user_agent =
       "HTTP_USER_AGENT=" + _request->getHeaderField("USER-AGENT");
-  std::string server_name =
-      "SERVER_=NAME" + _request->getMatchedServer()->getServerName();
-  std::string script_name =
-      "SCRIPT_NAME=" +
-      _request->getPath().substr(_request->getPath().find_last_of('/') + 1);
-  std::string server_software = "SERVER_SOFTWARE=HTTP/1.1";
-  std::string path_info = "PATH_INFO=" + _request->getPath();
-  std::string query_string = "QUERY_STRING=" + _request->getQueryString();
-  std::string remote_addr = "REMOTE_ADDR=" + _request->getHeaderField("HOST");
-  std::string request_method = "REQUEST_METHOD=" + _request->getMethod();
-  std::string script_filename = "SCRIPT_FILENAME=" + _request->getPath();
+  _server_name = "SERVER_=NAME" + _request->getMatchedServer()->getServerName();
+  _server_software = "SERVER_PROTOCOL=HTTP/1.1";
+  _path_info = "PATH_INFO=" + _request->getPath();
+  _query_string = "QUERY_STRING=" + _request->getQueryString();
+  _remote_addr = "REMOTE_ADDR=" + _request->getHeaderField("HOST");
+  _request_method = "REQUEST_METHOD=" + _request->getMethod();
+  _script_filename = "SCRIPT_FILENAME=" + _request->getPath();
 
-  if (_request->getMethod() == "POST") _env.push_back(content_length.c_str());
-  if (_request->getMethod() == "GET") _env.push_back(query_string.c_str());
-  _env.push_back(content_type.c_str());
-  _env.push_back(http_user_agent.c_str());
-  _env.push_back(path_info.c_str());
-  _env.push_back(remote_addr.c_str());
-  _env.push_back(script_filename.c_str());
-  _env.push_back(script_name.c_str());
-  _env.push_back(server_software.c_str());
-  _env.push_back(request_method.c_str());
-  _env.push_back(server_name.c_str());
-  _env.push_back(server_software.c_str());
+  if (_request->getMethod() == "POST") _env.push_back(_content_length.c_str());
+  if (_request->getMethod() == "GET") _env.push_back(_query_string.c_str());
+  _env.push_back(_content_type.c_str());
+  _env.push_back(_http_user_agent.c_str());
+  _env.push_back(_path_info.c_str());
+  _env.push_back(_remote_addr.c_str());
+  _env.push_back(_script_filename.c_str());
+  _env.push_back(_request_method.c_str());
+  _env.push_back(_server_name.c_str());
+  _env.push_back(_server_software.c_str());
 }
 
 void CGI::generateErrorResponse(Status status) {
   (void)status;
   _cgiFinishFlag = true;
-  // _response->setStatusCode(status);
-  // _response.createErrorResponse();
+  _response->setStatusCode(status);
+  _response->assembleResponse();
+  _response->setResponseParsed();
   Kqueue::enableEvent(_client_fd, EVFILT_WRITE, _client_info);
 };
 
 void CGI::generateResponse() {
   _cgiFinishFlag = true;
-  // 첫줄이 200 ok 출력되었을 때에만 response를 생성한다.
-  // generateErrorResponse(400);
-  // _response->setStatusCode(OK);
-  // _response->setBody(_cgiResult);
-  // _response->createResponse();
+  _cgiResult.replace(_cgiResult.find("Status:"), 7, "HTTP/1.1");
+  _response->setResponse(_cgiResult);
+  _response->setResponseParsed();
   Kqueue::enableEvent(_client_fd, EVFILT_WRITE, _client_info);
 }
 
@@ -143,6 +135,7 @@ void CGI::makeChild() {
     close(_out_pipe[0]);
     close(_out_pipe[1]);
     close(_in_pipe[0]);
+
     generateErrorResponse(INTERNAL_SERVER_ERROR);
   } else if (!_pid) {
     dup2(_in_pipe[0], STDIN_FILENO);
@@ -150,6 +143,7 @@ void CGI::makeChild() {
     close(_in_pipe[0]);
     close(_out_pipe[0]);
     close(_out_pipe[1]);
+
     execve(_request->getPath().c_str(), NULL, const_cast<char**>(_env.data()));
   }
   Kqueue::setFdSet(_out_pipe[0], FD_CGI);
@@ -221,6 +215,7 @@ void CGI::waitAndReadCGI() {
   Kqueue::deleteFdSet(_out_pipe[0], FD_CGI);
   Kqueue::deleteEvent(_out_pipe[0], EVFILT_READ);
   close(_out_pipe[0]);
+
   generateResponse();
 }
 
