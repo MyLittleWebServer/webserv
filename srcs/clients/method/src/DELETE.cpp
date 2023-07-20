8627e01ef6ab9353c6b92475ed9a975513230836
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2023-07-20 12:30:20 +0900

    feat(DELETE): implement delete method (#126)
    * feat(DELETE): implement delete
    
    * feat(DELETE): check if the path is directory

--- srcs/clients/method/src/DELETE.cpp
+++ srcs/clients/method/src/DELETE.cpp
@@ -3,15 +3,23 @@
 DELETE::DELETE() {}
 DELETE::~DELETE() {}
 
-void DELETE::doRequest(RequestDts& dts, IResponse &response) {
+void DELETE::doRequest(RequestDts& dts, IResponse& response) {
   (void)response;
-  if (std::remove(dts.path->c_str()) == false) {
+  struct stat fileInfo;
+  if (stat(dts.path->c_str(), &fileInfo) != 0)
+    throw(*dts.statusCode = E_404_NOT_FOUND);
+  if (fileInfo.st_mode & S_IFDIR) throw(*dts.statusCode = E_403_FORBIDDEN);
+  if (std::remove(dts.path->c_str()) != 0) {
+    std::cout << *dts.path << '\n';
     throw(*dts.statusCode = E_404_NOT_FOUND);
   }
   *dts.statusCode = E_200_OK;
 }
 
 void DELETE::createSuccessResponse(IResponse& response) {
+  response.addBody("Resource has been successfully deleted.\r\n");
+  response.setHeaderField("Content-Type", "text/plain");
+  response.setHeaderField("Content-Length", itos(response.getBody().size()));
   response.assembleResponse();
   response.setResponseParsed();
 }
