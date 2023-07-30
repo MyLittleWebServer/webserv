464142925a78732b21ff51db94899f546db0b510
middlefitting <wjdtmdcjf2@gmail.com>
2023-07-30 21:43:49 +0900

    fix(post): testerstop

--- srcs/clients/method/src/POST.cpp
+++ srcs/clients/method/src/POST.cpp
@@ -172,6 +172,8 @@ void POST::createSuccessResponse(IResponse& response) {
   response.setHeaderField("Location", _location);
   response.setBody("File has been successfully uploaded.\r\npath: /directory/" +
                    _title + "\r\n");
+  response.setHeaderField("Content-Type", "text/plain");
+  response.setHeaderField("Content-Length", itos(response.getBody().size()));
   response.assembleResponse();
   response.setResponseParsed();
 }
