4e3315f5c1e41d8f4f3df05ccbd7ff9e9d327151
KIM CHAN HEE <85754295+chanhihi@users.noreply.github.com>
2023-08-01 12:25:04 +0900

    test: import jwt (#222)

--- myforum/secure_cgi/jwt_cookie.py
+++ myforum/secure_cgi/jwt_cookie.py
@@ -1,7 +1,13 @@
+#!/Users/chanheki/.brew/bin/python3.11
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+sys.path.append("/Users/${HOME}/.brew/lib/python3.11/site-packages/pip")
+sys.path.append("/Users/${HOME}/.brew/lib/python3.11/site-packages/")
+
 import jwt
 import datetime
-import os
-
 
 class JwtCookie:
 	def __init__(self):
